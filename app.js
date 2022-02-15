f415b66200769d6c8a3b8a6241192b39c31645d8
Arahman75 <selfarahman01825@gmail.com>
2022-02-15 17:58:24 +0600

    added a new text

--- app.js
+++ app.js
@@ -1,41 +1,54 @@
+
+const searchFood =() =>{
+    const searchText = document.getElementById('search-field').value;
+    const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMeals(data.meals));
+}
+
+
+
+
 // fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
 // .then(res => res.json())
-// .then(data => displayMeals(data));
+// .then(data => displayMeals(data.meals));
 
-// const displayMeals = meals => {
-//     const mealsDiv = document.getElementById('meals');
-//     for (let i = 0; i < meals.length; i++) {
-//         const meal = meals[i];
-//         const mealDiv = document.createElement('ul');
-//        const h1 =document.createElement('h1');
-//        h1.innerText = meal.strMeal;
-//        mealDiv.appendChild(h1);
-//        mealsDiv.appendChild(mealDiv);
+
+const displayMeals = meals => {
+    const mealsDiv = document.getElementById('meals');
+    for (let i = 0; i < meals.length; i++) {
+        const meal = meals[i];
+        const mealDiv = document.createElement('div');
+       const h1 =document.createElement('h1');
+       h1.innerText = meal.strMeal;
+       mealDiv.appendChild(h1);
+       mealsDiv.appendChild(mealDiv);
         
-//     }
+    }
     
 
-// }
-
-const createMealInfoDiv = (meal, mealInput) => {
-    const mealPhoto = meal.strMealThumb;
-    const mealName = meal.strMeal;
-    const mealInfo = `
-    <a href="#meal-details-section" style="text-decoration: none; color: black;">
-        <div onclick="getMealDetails(${meal.idMeal})" class="card border-0 shadow cursor" style="width: 18rem; border-radius: 10px">
-            <img src="${mealPhoto}" class="card-img-top" style="width: 18rem; border-radius: 10px 10px 0 0" alt="...">
-            <div class="card-body">
-                <h5 class="card-title text-center">${mealName}</h5>
-            </div>
-        </div>
-    </a>
-    `
-    const mealInfoSection = document.getElementById('meal-info-section');
-    const mealInfoDiv = document.createElement('div');
-    mealInfoDiv.className = 'col-xm-1 col-sm-1 col-md-3 p-3 d-flex justify-content-center';
-    mealInfoDiv.innerHTML = mealInfo;
-    mealInfoSection.appendChild(mealInfoDiv);
 }
+
+// const createMealInfoDiv = (meal, mealInput) => {
+//     const mealPhoto = meal.strMealThumb;
+//     const mealName = meal.strMeal;
+//     const mealInfo = `
+//     <a href="#meal-details-section" style="text-decoration: none; color: black;">
+//         <div onclick="getMealDetails(${meal.idMeal})" class="card border-0 shadow cursor" style="width: 18rem; border-radius: 10px">
+//             <img src="${mealPhoto}" class="card-img-top" style="width: 18rem; border-radius: 10px 10px 0 0" alt="...">
+//             <div class="card-body">
+//                 <h5 class="card-title text-center">${mealName}</h5>
+//             </div>
+//         </div>
+//     </a>
+//     `
+//     const mealInfoSection = document.getElementById('meal-info-section');
+//     const mealInfoDiv = document.createElement('div');
+//     mealInfoDiv.className = 'col-xm-1 col-sm-1 col-md-3 p-3 d-flex justify-content-center';
+//     mealInfoDiv.innerHTML = mealInfo;
+//     mealInfoSection.appendChild(mealInfoDiv);
+// }
 
 
 
